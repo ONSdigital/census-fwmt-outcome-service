1de06bef37d7052200bc448d00f639600038dfbb
Phil Sharland <philip.sharland@ons.gov.uk>
2019-02-18 10:57:29 +0000

    Initial Commit

---
+++ src/main/java/uk/gov/ons/fwmt/census/feedback/Application.java
@@ -0,0 +1,13 @@
+package uk.gov.ons.fwmt.census.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
