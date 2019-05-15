4d15b3a1f56e4b8fc0b13689fc9985930cb5788a
jacobharrison <jacob.harrison@qa.com>
2019-05-15 09:58:22 +0100

    clean up of logic in fulfillment factory

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/factory/FulfilmentRequestFactory.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/factory/FulfilmentRequestFactory.java
@@ -9,6 +9,7 @@ import uk.gov.ons.census.fwmt.outcomeservice.data.dto.rm.Event;
 import uk.gov.ons.census.fwmt.outcomeservice.data.dto.rm.Fulfillment;
 import uk.gov.ons.census.fwmt.outcomeservice.data.dto.rm.OutcomeEvent;
 import uk.gov.ons.census.fwmt.outcomeservice.data.dto.rm.Payload;
+import uk.gov.ons.census.fwmt.outcomeservice.data.dto.rm.Uac;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -37,42 +38,68 @@ public class FulfilmentRequestFactory {
   }
 
   public OutcomeEvent[] createFulfilmentEvents(HouseholdOutcome householdOutcome) throws GatewayException {
-    return newFulfilmentRequestList(householdOutcome);
+    return getFulfilmentRequest(householdOutcome);
   }
 
-  private OutcomeEvent[] newFulfilmentRequestList(HouseholdOutcome householdOutcome) throws GatewayException {
+  private OutcomeEvent buildFulfilmentPayload(HouseholdOutcome householdOutcome) {
     OutcomeEvent outcomeEvent = new OutcomeEvent();
     Payload payload = new Payload();
     Fulfillment fulfillment = new Fulfillment();
     Contact contact = new Contact();
 
     fulfillment.setContact(contact);
+    fulfillment.setCaseId(householdOutcome.getCaseId());
     payload.setFulfillment(fulfillment);
 
     outcomeEvent.setPayload(payload);
 
     Event event = new Event();
+    event.setType("FULFILMENT_REQUESTED");
     event.setSource("FIELDWORK_GATEWAY");
     event.setChannel("FIELD");
-    event.setType("FULFILMENT_REQUESTED");
     event.setTransactionId(householdOutcome.getTransactionId());
     event.setDateTime(householdOutcome.getEventDate());
     outcomeEvent.setEvent(event);
 
-    return getFulfilmentRequest(householdOutcome, outcomeEvent);
+    return outcomeEvent;
   }
 
-  private OutcomeEvent[] getFulfilmentRequest(HouseholdOutcome householdOutcome, OutcomeEvent outcomeEvent)
+  private OutcomeEvent buildUacPayload(HouseholdOutcome householdOutcome) {
+    OutcomeEvent outcomeEvent = new OutcomeEvent();
+    Payload payload = new Payload();
+    Uac uac = new Uac();
+
+
+    uac.setCaseId(householdOutcome.getCaseId());
+    payload.setUac(uac);
+
+    outcomeEvent.setPayload(payload);
+
+    Event event = new Event();
+    event.setType("QUESTIONNAIRE_LINKED");
+    event.setSource("FIELDWORK_GATEWAY");
+    event.setChannel("FIELD");
+    event.setTransactionId(householdOutcome.getTransactionId());
+    event.setDateTime(householdOutcome.getEventDate());
+    outcomeEvent.setEvent(event);
+
+    return outcomeEvent;
+  }
+
+
+  private OutcomeEvent[] getFulfilmentRequest(HouseholdOutcome householdOutcome)
       throws GatewayException {
     List<OutcomeEvent> outcomeEventList = new ArrayList<>();
     for (FulfillmentRequest fulfillmentRequest : householdOutcome.getFulfillmentRequests()) {
-      outcomeEvent.getPayload().getFulfillment().setCaseId(householdOutcome.getCaseId());
+      OutcomeEvent outcomeEvent;
       switch (householdOutcome.getSecondaryOutcome()) {
       case "Paper Questionnaire required by post":
-        getQuestionnaireByPost(outcomeEvent, outcomeEventList, fulfillmentRequest);
+        outcomeEvent = buildFulfilmentPayload(householdOutcome);
+        outcomeEventList.add(getQuestionnaireByPost(outcomeEvent, fulfillmentRequest));
         break;
       case "UAC required by text":
-        getUacByText(outcomeEvent, outcomeEventList, fulfillmentRequest);
+        outcomeEvent = buildFulfilmentPayload(householdOutcome);
+        outcomeEventList.add(getUacByText(outcomeEvent, fulfillmentRequest));
         break;
       case "Paper H Questionnaire issued":
       case "Will Complete":
@@ -82,9 +109,8 @@ public class FulfilmentRequestFactory {
       case "Holiday home":
       case "Second residence":
       case "Requested assistance":
-        outcomeEvent.getPayload().getUac().setCaseId(householdOutcome.getCaseId());
+        outcomeEvent = buildUacPayload(householdOutcome);
         outcomeEvent.getPayload().getUac().setQuestionnaireId(fulfillmentRequest.getQuestionnaireId());
-
         outcomeEventList.add(outcomeEvent);
         break;
       default:
@@ -95,32 +121,7 @@ public class FulfilmentRequestFactory {
     return outcomeEventList.toArray(new OutcomeEvent[0]);
   }
 
-  private void getUacByText(OutcomeEvent outcomeEvent, List<OutcomeEvent> outcomeEventList, FulfillmentRequest fulfillmentRequest) throws GatewayException {
-    if (householdUacMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
-
-      outcomeEvent.getPayload().getFulfillment()
-          .setProductCode(householdUacMap.get(fulfillmentRequest.getQuestionnaireType()));
-
-      outcomeEvent.getPayload().getFulfillment().getContact().setTelNo(fulfillmentRequest.getRequesterPhone());
-
-      outcomeEventList.add(outcomeEvent);
-
-    } else if (individualUacMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
-
-      outcomeEvent.getPayload().getFulfillment()
-          .setProductCode(individualUacMap.get(fulfillmentRequest.getQuestionnaireType()));
-
-      outcomeEvent.getPayload().getFulfillment().getContact().setTelNo(fulfillmentRequest.getRequesterPhone());
-
-      outcomeEventList.add(outcomeEvent);
-
-    } else {
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-          "Failed to find valid Questionnaire Type " + fulfillmentRequest.getQuestionnaireType());
-    }
-  }
-
-  private void getQuestionnaireByPost(OutcomeEvent outcomeEvent, List<OutcomeEvent> outcomeEventList,
+  private OutcomeEvent getQuestionnaireByPost(OutcomeEvent outcomeEvent,
       FulfillmentRequest fulfillmentRequest) throws GatewayException {
 
     if (householdPaperMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
@@ -128,13 +129,10 @@ public class FulfilmentRequestFactory {
       outcomeEvent.getPayload().getFulfillment()
           .setProductCode(householdPaperMap.get(fulfillmentRequest.getQuestionnaireType()));
 
-      outcomeEventList.add(outcomeEvent);
     } else if (householdContinuationMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
 
       outcomeEvent.getPayload().getFulfillment()
           .setProductCode(householdContinuationMap.get(fulfillmentRequest.getQuestionnaireType()));
-
-      outcomeEventList.add(outcomeEvent);
 
     } else if (householdIndividualMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
 
@@ -145,11 +143,32 @@ public class FulfilmentRequestFactory {
       outcomeEvent.getPayload().getFulfillment().getContact().setForename(fulfillmentRequest.getRequesterForename());
       outcomeEvent.getPayload().getFulfillment().getContact().setSurname(fulfillmentRequest.getRequesterSurname());
 
-      outcomeEventList.add(outcomeEvent);
+    } else {
+      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
+          "Failed to find valid Questionnaire Type " + fulfillmentRequest.getQuestionnaireType());
+    }
+    return outcomeEvent;
+  }
+
+  private OutcomeEvent getUacByText(OutcomeEvent outcomeEvent, FulfillmentRequest fulfillmentRequest) throws GatewayException {
+    if (householdUacMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
+
+      outcomeEvent.getPayload().getFulfillment()
+          .setProductCode(householdUacMap.get(fulfillmentRequest.getQuestionnaireType()));
+
+      outcomeEvent.getPayload().getFulfillment().getContact().setTelNo(fulfillmentRequest.getRequesterPhone());
+
+    } else if (individualUacMap.containsKey(fulfillmentRequest.getQuestionnaireType())) {
+
+      outcomeEvent.getPayload().getFulfillment()
+          .setProductCode(individualUacMap.get(fulfillmentRequest.getQuestionnaireType()));
+
+      outcomeEvent.getPayload().getFulfillment().getContact().setTelNo(fulfillmentRequest.getRequesterPhone());
 
     } else {
       throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
           "Failed to find valid Questionnaire Type " + fulfillmentRequest.getQuestionnaireType());
     }
+    return outcomeEvent;
   }
 }
