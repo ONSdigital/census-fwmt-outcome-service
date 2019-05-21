bf0b614e0e00c00858ffb62ca22dc3a38d6b82e8
jacobharrison <jacob.harrison@qa.com>
2019-05-21 16:35:58 +0100

    added Swagger config and documented outcome controller

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/controller/OutcomeController.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/controller/OutcomeController.java
@@ -1,5 +1,9 @@
 package uk.gov.ons.census.fwmt.outcomeservice.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +20,7 @@ import static uk.gov.ons.census.fwmt.outcomeservice.config.GatewayEventsConfig.C
 
 @RestController
 @RequestMapping("/OutcomeController")
+@Api(value = "FWMT Census Outcome Service", description = "Operations pertaining to receiving outcomes from COMET")
 public class OutcomeController {
 
   @Autowired
@@ -24,6 +29,13 @@ public class OutcomeController {
   @Autowired
   private GatewayEventManager gatewayEventManager;
 
+  @ApiOperation(value = "Post an outcome to the FWMT Gateway")
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Outcome successfully received"),
+      @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+      @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+      @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+  })
   @PostMapping(value = "/HouseholdOutcome", consumes = "application/json", produces = "application/json")
   public void householdCaseOutcomeResponse(@RequestBody HouseholdOutcome householdOutcome) throws GatewayException {
     gatewayEventManager
