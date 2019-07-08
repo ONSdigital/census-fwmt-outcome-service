32e8b84f7f8631996629e6f0aa8e80097cc17846
Simon <sdiaz1000@gmail.com>
2019-07-08 16:44:03 +0100

    fwmt-1220 Files missed from last commit

--- src/test/java/uk/gov/ons/census/fwmt/outcomeservice/helper/OutcomeEventFactoryBuilder.java
+++ src/test/java/uk/gov/ons/census/fwmt/outcomeservice/helper/OutcomeEventFactoryBuilder.java
@@ -20,78 +20,78 @@ public class OutcomeEventFactoryBuilder {
 
   static Map<String, String> noValidHouseholdOutcomeMap = new HashMap<>();
 
-  public static OutcomeEvent createOutcomeEventForHousehold(HouseholdOutcome householdOutcome) {
-    OutcomeEvent outcomeEvent = new OutcomeEvent();
-    Payload payload = new Payload();
-    InvalidAddress invalidAddress = new InvalidAddress();
-    Refusal refusal = new Refusal();
-    Contact contact = new Contact();
-    CollectionCase collectionCase = new CollectionCase();
-
-    collectionCase.setId(householdOutcome.getCaseId());
-    invalidAddress.setCollectionCase(collectionCase);
-    refusal.setCollectionCase(collectionCase);
-    payload.setContact(contact);
-    payload.setRefusal(refusal);
-    payload.setInvalidAddress(invalidAddress);
-
-    outcomeEvent.setPayload(payload);
-
-    Event event = new Event();
-    event.setSource("FIELDWORK_GATEWAY");
-    event.setChannel("FIELD");
-    event.setTransactionId(householdOutcome.getTransactionId());
-    event.setDateTime(householdOutcome.getEventDate());
-    outcomeEvent.setEvent(event);
-
-    outcomeEvent.getEvent().setType("ADDRESS_NOT_VALID");
-    outcomeEvent.getPayload().getInvalidAddress()
-        .setReason(noValidHouseholdOutcomeMap.get(householdOutcome.getSecondaryOutcome()));
-    outcomeEvent.getPayload().getInvalidAddress().getCollectionCase().setId(householdOutcome.getCaseId());
-
-    return outcomeEvent;
-  }
-
-  public static OutcomeEvent createOutcomeEventForHouseholdRefusal(HouseholdOutcome householdOutcome) {
-    OutcomeEvent outcomeEvent = new OutcomeEvent();
-    Payload payload = new Payload();
-    InvalidAddress invalidAddress = new InvalidAddress();
-    Refusal refusal = new Refusal();
-    Contact contact = new Contact();
-    CollectionCase collectionCase = new CollectionCase();
-
-    collectionCase.setId(householdOutcome.getCaseId());
-    invalidAddress.setCollectionCase(collectionCase);
-    refusal.setCollectionCase(collectionCase);
-    payload.setContact(contact);
-    payload.setRefusal(refusal);
-    payload.setInvalidAddress(invalidAddress);
-
-    outcomeEvent.setPayload(payload);
-
-    Event event = new Event();
-    event.setSource("FIELDWORK_GATEWAY");
-    event.setChannel("FIELD");
-    event.setTransactionId(householdOutcome.getTransactionId());
-    event.setDateTime(householdOutcome.getEventDate());
-    outcomeEvent.setEvent(event);
-
-    outcomeEvent.getEvent().setType("REFUSAL_RECEIVED");
-    outcomeEvent.getPayload().getInvalidAddress()
-        .setReason(noValidHouseholdOutcomeMap.get(householdOutcome.getSecondaryOutcome()));
-    outcomeEvent.getPayload().getInvalidAddress().getCollectionCase().setId(householdOutcome.getCaseId());
-
-    return outcomeEvent;
-  }
-  
-  public static void main(String[] args) throws JsonProcessingException {
-  ObjectMapper mapper = new ObjectMapper();  
-    HouseholdOutcome householdOutcome = new HouseholdOutcomeBuilder().createContactMadeHouseholdHardRefusal();
-    OutcomeServiceImpl.createOutcomeEventText(householdOutcome);
-    
-//    OutcomeEvent createOutcomeEventForHouseholdRefusal = createOutcomeEventForHouseholdRefusal(householdOutcome);
-//    String ho = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(createOutcomeEventForHouseholdRefusal);
-//    System.out.println(ho);
-
-  }
+//  public static OutcomeEvent createOutcomeEventForHousehold(HouseholdOutcome householdOutcome) {
+//    OutcomeEvent outcomeEvent = new OutcomeEvent();
+//    Payload payload = new Payload();
+//    InvalidAddress invalidAddress = new InvalidAddress();
+//    Refusal refusal = new Refusal();
+//    Contact contact = new Contact();
+//    CollectionCase collectionCase = new CollectionCase();
+//
+//    collectionCase.setId(householdOutcome.getCaseId());
+//    invalidAddress.setCollectionCase(collectionCase);
+//    refusal.setCollectionCase(collectionCase);
+//    payload.setContact(contact);
+//    payload.setRefusal(refusal);
+//    payload.setInvalidAddress(invalidAddress);
+//
+//    outcomeEvent.setPayload(payload);
+//
+//    Event event = new Event();
+//    event.setSource("FIELDWORK_GATEWAY");
+//    event.setChannel("FIELD");
+//    event.setTransactionId(householdOutcome.getTransactionId());
+//    event.setDateTime(householdOutcome.getEventDate());
+//    outcomeEvent.setEvent(event);
+//
+//    outcomeEvent.getEvent().setType("ADDRESS_NOT_VALID");
+//    outcomeEvent.getPayload().getInvalidAddress()
+//        .setReason(noValidHouseholdOutcomeMap.get(householdOutcome.getSecondaryOutcome()));
+//    outcomeEvent.getPayload().getInvalidAddress().getCollectionCase().setId(householdOutcome.getCaseId());
+//
+//    return outcomeEvent;
+//  }
+//
+//  public static OutcomeEvent createOutcomeEventForHouseholdRefusal(HouseholdOutcome householdOutcome) {
+//    OutcomeEvent outcomeEvent = new OutcomeEvent();
+//    Payload payload = new Payload();
+//    InvalidAddress invalidAddress = new InvalidAddress();
+//    Refusal refusal = new Refusal();
+//    Contact contact = new Contact();
+//    CollectionCase collectionCase = new CollectionCase();
+//
+//    collectionCase.setId(householdOutcome.getCaseId());
+//    invalidAddress.setCollectionCase(collectionCase);
+//    refusal.setCollectionCase(collectionCase);
+//    payload.setContact(contact);
+//    payload.setRefusal(refusal);
+//    payload.setInvalidAddress(invalidAddress);
+//
+//    outcomeEvent.setPayload(payload);
+//
+//    Event event = new Event();
+//    event.setSource("FIELDWORK_GATEWAY");
+//    event.setChannel("FIELD");
+//    event.setTransactionId(householdOutcome.getTransactionId());
+//    event.setDateTime(householdOutcome.getEventDate());
+//    outcomeEvent.setEvent(event);
+//
+//    outcomeEvent.getEvent().setType("REFUSAL_RECEIVED");
+//    outcomeEvent.getPayload().getInvalidAddress()
+//        .setReason(noValidHouseholdOutcomeMap.get(householdOutcome.getSecondaryOutcome()));
+//    outcomeEvent.getPayload().getInvalidAddress().getCollectionCase().setId(householdOutcome.getCaseId());
+//
+//    return outcomeEvent;
+//  }
+//  
+//  public static void main(String[] args) throws JsonProcessingException {
+//  ObjectMapper mapper = new ObjectMapper();  
+//    HouseholdOutcome householdOutcome = new HouseholdOutcomeBuilder().createContactMadeHouseholdHardRefusal();
+//    OutcomeServiceImpl.createOutcomeEventText(householdOutcome);
+//    
+////    OutcomeEvent createOutcomeEventForHouseholdRefusal = createOutcomeEventForHouseholdRefusal(householdOutcome);
+////    String ho = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(createOutcomeEventForHouseholdRefusal);
+////    System.out.println(ho);
+//
+//  }
 }
