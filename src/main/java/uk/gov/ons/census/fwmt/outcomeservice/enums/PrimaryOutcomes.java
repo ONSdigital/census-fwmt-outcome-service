8c68322a8988b388f29d3c8dcfe817e1a62415e8
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-07-23 11:34:05 +0100

    Update to primary outcomes to match mapping

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/enums/PrimaryOutcomes.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/enums/PrimaryOutcomes.java
@@ -2,7 +2,7 @@ package uk.gov.ons.census.fwmt.outcomeservice.enums;
 
 public enum PrimaryOutcomes {
 
-  NON_VALID_HOUSEHOLD("Non-valid Household"),
+  NON_VALID_HOUSEHOLD("Non-valid household"),
   CONTACT_MADE("Contact made");
 
   private String value;
