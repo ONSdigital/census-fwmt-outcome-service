0c9cfa4304cb7ea46a3fd3e1b93c9f4c3011b381
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-10-23 13:34:49 +0100

    Added logging for RabbitMQ and Redis health checks

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/config/GatewayEventsConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/config/GatewayEventsConfig.java
@@ -24,14 +24,20 @@ public class GatewayEventsConfig {
   public static final String FAILED_FULFILMENT_REQUEST_IS_NULL = "FAILED_FULFILMENT_REQUEST_IS_NULL";
   public static final String CCS_FAILED_FULFILMENT_REQUEST_INVALID = "FAILED_FULFILMENT_REQUEST_SIZE_INVALID";
 
+  //Health logging
+  public static final String RABBIT_QUEUE_UP = "RABBIT_QUEUE_UP";
+  public static final String RABBIT_QUEUE_DOWN = "RABBIT_QUEUE_DOWN";
+  public static final String REDIS_SERVICE_UP = "REDIS_SERVICE_UP";
+  public static final String REDIS_SERVICE_DOWN = "REDIS_SERVICE_DOWN";
+
   @Bean
   public GatewayEventManager gatewayEventManager() {
     GatewayEventManager gatewayEventManager = new GatewayEventManager();
     gatewayEventManager.setSource(Application.APPLICATION_NAME);
     gatewayEventManager.addEventTypes(new String[] {COMET_HH_OUTCOME_RECEIVED, COMET_CCSSI_OUTCOME_RECEIVED,
-        COMET_CCSPL_OUTCOME_RECEIVED, CCSPL_OUTCOME_SENT, HH_OUTCOME_SENT, CCSI_OUTCOME_SENT});
+        COMET_CCSPL_OUTCOME_RECEIVED, CCSPL_OUTCOME_SENT, HH_OUTCOME_SENT, CCSI_OUTCOME_SENT, RABBIT_QUEUE_UP, REDIS_SERVICE_UP});
     gatewayEventManager.addErrorEventTypes(new String[] {FAILED_JSON_CONVERSION, FAILED_FULFILMENT_REQUEST_IS_NULL,
-        CCS_FAILED_FULFILMENT_REQUEST_INVALID});
+        CCS_FAILED_FULFILMENT_REQUEST_INVALID, RABBIT_QUEUE_DOWN, REDIS_SERVICE_DOWN});
     
     return gatewayEventManager;
   }
