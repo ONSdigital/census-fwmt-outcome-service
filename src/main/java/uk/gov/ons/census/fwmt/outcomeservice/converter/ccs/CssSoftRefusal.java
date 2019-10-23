b08f05b3db7bcbcb36408ad5d86da912bbf3a7d0
SteveScorfield <steve.scorfield@ons.gov.uk>
2019-10-23 14:56:23 +0100

    Added new soft refusal template and removed soft refusal as a category from refusals

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/ccs/CssSoftRefusal.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/ccs/CssSoftRefusal.java
@@ -1,21 +1,7 @@
 package uk.gov.ons.census.fwmt.outcomeservice.converter.ccs;
 
-import static uk.gov.ons.census.fwmt.outcomeservice.config.GatewayEventsConfig.CCSPL_OUTCOME_SENT;
-import static uk.gov.ons.census.fwmt.outcomeservice.enums.EventType.REFUSAL_RECEIVED;
-import static uk.gov.ons.census.fwmt.outcomeservice.enums.SurveyType.ccs;
-import static uk.gov.ons.census.fwmt.outcomeservice.util.CcsUtilityMethods.getAddressLevel;
-import static uk.gov.ons.census.fwmt.outcomeservice.util.CcsUtilityMethods.getAddressType;
-import static uk.gov.ons.census.fwmt.outcomeservice.util.CcsUtilityMethods.getOrganisationName;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import uk.gov.ons.census.fwmt.common.data.ccs.CCSPropertyListingOutcome;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
@@ -24,8 +10,15 @@ import uk.gov.ons.census.fwmt.outcomeservice.message.GatewayOutcomeProducer;
 import uk.gov.ons.census.fwmt.outcomeservice.service.JobCacheManager;
 import uk.gov.ons.census.fwmt.outcomeservice.template.TemplateCreator;
 
+import java.util.*;
+
+import static uk.gov.ons.census.fwmt.outcomeservice.config.GatewayEventsConfig.CCSPL_OUTCOME_SENT;
+import static uk.gov.ons.census.fwmt.outcomeservice.enums.EventType.SOFT_REFUSAL;
+import static uk.gov.ons.census.fwmt.outcomeservice.enums.SurveyType.ccs;
+import static uk.gov.ons.census.fwmt.outcomeservice.util.CcsUtilityMethods.*;
+
 @Component
-public class CcsRefusalReceivedProcessor implements CcsOutcomeServiceProcessor {
+public class CssSoftRefusal implements CcsOutcomeServiceProcessor {
 
   @Autowired
   private GatewayOutcomeProducer gatewayOutcomeProducer;
@@ -38,18 +31,16 @@ public class CcsRefusalReceivedProcessor implements CcsOutcomeServiceProcessor {
 
   @Override
   public boolean isValid(CCSPropertyListingOutcome ccsPLOutcome) {
-    List<String> validSecondaryOutcomes = Arrays.asList("Hard refusal", "Extraordinary refusal");
+    List<String> validSecondaryOutcomes = Collections.singletonList("Soft refusal");
+
     return validSecondaryOutcomes.contains(ccsPLOutcome.getSecondaryOutcome());
   }
 
   @Override
   public void processMessage(CCSPropertyListingOutcome ccsPLOutcome) throws GatewayException {
     UUID newRandomUUID = UUID.randomUUID();
-    if(ccsPLOutcome.getSecondaryOutcome().equals("Soft refusal")) {
-      jobCacheManager.cacheCCSOutcome(String.valueOf(newRandomUUID), ccsPLOutcome);
-    }
+    jobCacheManager.cacheCCSOutcome(String.valueOf(newRandomUUID), ccsPLOutcome);
 
-    CcsSecondaryOutcomeMap ccsSecondaryOutcomeMap = new CcsSecondaryOutcomeMap();
     String eventDateTime = ccsPLOutcome.getEventDate().toString();
     Map<String, Object> root = new HashMap<>();
     root.put("generatedUuid", newRandomUUID);
@@ -57,13 +48,12 @@ public class CcsRefusalReceivedProcessor implements CcsOutcomeServiceProcessor {
     root.put("addressType", getAddressType(ccsPLOutcome));
     root.put("addressLevel", getAddressLevel(ccsPLOutcome));
     root.put("organisationName", getOrganisationName(ccsPLOutcome));
-    root.put("refusalType", ccsSecondaryOutcomeMap.ccsSecondaryOutcomeMap.get(ccsPLOutcome.getSecondaryOutcome()));
     root.put("eventDate", eventDateTime + "Z");
 
-    String outcomeEvent = TemplateCreator.createOutcomeMessage(REFUSAL_RECEIVED, root, ccs);
+    String outcomeEvent = TemplateCreator.createOutcomeMessage(SOFT_REFUSAL, root, ccs);
 
     gatewayOutcomeProducer.sendPropertyListing(outcomeEvent, String.valueOf(ccsPLOutcome.getTransactionId()));
     gatewayEventManager.triggerEvent(String.valueOf(ccsPLOutcome.getPropertyListingCaseId()), CCSPL_OUTCOME_SENT,
-        "type", "CCSPL_REFUSAL_RECEIVED_OUTCOME_SENT", "transactionId", ccsPLOutcome.getTransactionId().toString());
+        "type", "CCSPL_SOFT_REFUSAL_OUTCOME_SENT", "transactionId", ccsPLOutcome.getTransactionId().toString());
   }
 }
