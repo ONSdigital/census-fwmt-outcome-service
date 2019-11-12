595ec6d9bc83cc953e92553582d42190d3363939
Simon <sdiaz1000@gmail.com>
2019-11-12 11:11:48 +0000

    force a travis build

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/util/CcsUtilityMethods.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/util/CcsUtilityMethods.java
@@ -37,6 +37,7 @@ public final class CcsUtilityMethods {
     return "";
   }
 
+  
   public static String getOrganisationName(CCSPropertyListingOutcome ccsPropertyListingOutcome) {
     if (ccsPropertyListingOutcome.getCeDetails() != null) {
       if (!StringUtils.isEmpty(ccsPropertyListingOutcome.getCeDetails().getOrganisationName())) {
