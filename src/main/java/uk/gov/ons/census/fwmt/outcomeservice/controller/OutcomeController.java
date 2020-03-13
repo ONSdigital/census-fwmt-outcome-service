08b7451ae729a084280860ab9ba3cfb239d4493b
jacobharrison <jacobharrison@outlook.com>
2020-03-13 12:32:01 +0000

    changes

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/controller/OutcomeController.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/controller/OutcomeController.java
@@ -1,7 +1,5 @@
 package uk.gov.ons.census.fwmt.outcomeservice.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
@@ -28,95 +26,15 @@ public class OutcomeController implements OutcomeApi {
   @Autowired
   private OutcomePreprocessingProducer outcomePreprocessingProducer;
 
-  //  @Override
-  //  public ResponseEntity<Void> householdCaseOutcomeResponse(String caseId, HouseholdOutcome householdOutcome)
-  //      throws GatewayException {
-  //    gatewayEventManager.triggerEvent(caseId, COMET_HH_OUTCOME_RECEIVED, "transactionId",
-  //        householdOutcome.getTransactionId().toString(), "Case Ref", householdOutcome.getCaseReference(),
-  //        "Primary Outcome", householdOutcome.getPrimaryOutcome(), "Secondary Outcome",
-  //        householdOutcome.getSecondaryOutcome());
-  //    householdOutcome.setCaseId(UUID.fromString(caseId));
-  //
-  //    try {
-  //      String householdOutcomeToQueue = objectMapper.writeValueAsString(householdOutcome);
-  //      outcomeType = "Household";
-  //      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(householdOutcomeToQueue, caseId, outcomeType);
-  //    } catch (JsonProcessingException e) {
-  //      String errorMessage = "Unable to move household outcome to pre-processing queue.";
-  //      gatewayEventManager
-  //          .triggerErrorEvent(this.getClass(), e, errorMessage, caseId, FAILED_JSON_CONVERSION, "Case Ref",
-  //              householdOutcome.getCaseReference());
-  //      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-  //          errorMessage + " for case Id: " + caseId);
-  //    }
-  //    return new ResponseEntity<>(HttpStatus.ACCEPTED);
-  //  }
-  //
-  //  @Override
-  //  public ResponseEntity<Void> ccsPropertyListingCaseOutcomeResponse(
-  //      CCSPropertyListingOutcome ccsPLOutcome) throws GatewayException {
-  //    String caseId = ccsPLOutcome.getPropertyListingCaseId().toString();
-  //    gatewayEventManager
-  //        .triggerEvent(caseId, COMET_CCSPL_OUTCOME_RECEIVED, "transactionId", ccsPLOutcome.getTransactionId().toString(),
-  //            "Primary Outcome", ccsPLOutcome.getPrimaryOutcome(), "Secondary Outcome",
-  //            ccsPLOutcome.getSecondaryOutcome());
-  //
-  //    try {
-  //      String ccsPLOutcomeToQueue = objectMapper.writeValueAsString(ccsPLOutcome);
-  //      outcomeType = "CCSPL";
-  //      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(ccsPLOutcomeToQueue, caseId, outcomeType);
-  //    } catch (JsonProcessingException e) {
-  //      String errorMessage = "Unable to move household outcome to pre-processing queue.";
-  //      gatewayEventManager.triggerErrorEvent(this.getClass(), e, errorMessage, caseId, FAILED_JSON_CONVERSION);
-  //      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-  //          errorMessage + " for case Id: " + caseId);
-  //    }
-  //    return new ResponseEntity<>(HttpStatus.ACCEPTED);
-  //  }
-  //
-  //  @Override
-  //  public ResponseEntity<Void> ccsInterviewOutcome(String caseId, CCSInterviewOutcome ccsInterviewOutcome)
-  //      throws GatewayException {
-  //    gatewayEventManager.triggerEvent(caseId, COMET_CCSSI_OUTCOME_RECEIVED, "transactionId",
-  //        ccsInterviewOutcome.getTransactionId().toString(), "Case Ref", ccsInterviewOutcome.getCaseReference(),
-  //        "Primary Outcome", ccsInterviewOutcome.getPrimaryOutcome(), "Secondary Outcome",
-  //        ccsInterviewOutcome.getSecondaryOutcome());
-  //
-  //    try {
-  //      String ccsInterviewOutcomeToQueue = objectMapper.writeValueAsString(ccsInterviewOutcome);
-  //      outcomeType = "CCSINT";
-  //      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(ccsInterviewOutcomeToQueue, caseId, outcomeType);
-  //    } catch (JsonProcessingException e) {
-  //      String errorMessage = "Unable to move household outcome to pre-processing queue.";
-  //      gatewayEventManager
-  //          .triggerErrorEvent(this.getClass(), e, errorMessage, caseId, FAILED_JSON_CONVERSION, "Case Ref",
-  //              ccsInterviewOutcome.getCaseReference());
-  //      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-  //          errorMessage + " for case Id: " + caseId);
-  //    }
-  //    return new ResponseEntity<>(HttpStatus.ACCEPTED);
-  //  }
-
   @Override
-  public ResponseEntity<Void> spgOutcomeResponse(String caseId, SPGOutcome spgOutcome)
-      throws GatewayException {
+  public ResponseEntity<Void> spgOutcomeResponse(String caseId, SPGOutcome spgOutcome) {
     gatewayEventManager.triggerEvent(caseId, COMET_CESPG_OUTCOME_RECEIVED, "transactionId",
         spgOutcome.getTransactionId().toString(), "Primary Outcome", spgOutcome.getPrimaryOutcomeDescription(),
         "Secondary Outcome",
         spgOutcome.getSecondaryOutcomeDescription());
 
-    try {
-      String spgOutcomeToQueue = objectMapper.writeValueAsString(spgOutcome);
-      outcomeType = "SPG";
-      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(spgOutcomeToQueue, caseId, outcomeType);
-    } catch (JsonProcessingException e) {
-      String errorMessage = "Unable to move SPG outcome to pre-processing queue.";
-      gatewayEventManager
-          .triggerErrorEvent(this.getClass(), e, errorMessage, caseId, FAILED_JSON_CONVERSION, "Case Ref",
-              spgOutcome.getCaseReference());
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-          errorMessage + " for case Id: " + caseId);
-    }
+    outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(spgOutcome,"");
+
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
@@ -124,22 +42,10 @@ public class OutcomeController implements OutcomeApi {
   @Override
   public ResponseEntity<Void> spgNewUnitAddress(NewUnitAddress spgOutcome) {
     gatewayEventManager.triggerEvent("", COMET_CESPGUNITADDRESS_OUTCOME_RECEIVED, "transactionId",
-        spgOutcome.getTransactionId().toString(), "Case Ref", spgOutcome.getCaseReference(),
-        "Primary Outcome", spgOutcome.getPrimaryOutcomeDescription(), "Secondary Outcome",
+        spgOutcome.getTransactionId().toString(),"Primary Outcome", spgOutcome.getPrimaryOutcomeDescription(), "Secondary Outcome",
         spgOutcome.getSecondaryOutcomeDescription());
 
-    try {
-      String spgOutcomeToQueue = objectMapper.writeValueAsString(spgOutcome);
-      outcomeType = "SPGUA";
-      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(spgOutcomeToQueue, "", outcomeType);
-    } catch (JsonProcessingException e) {
-      String errorMessage = "Unable to move SPG Unit Address outcome to pre-processing queue.";
-      gatewayEventManager
-          .triggerErrorEvent(this.getClass(), e, errorMessage, "", FAILED_JSON_CONVERSION, "Case Ref",
-              spgOutcome.getCaseReference());
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-          errorMessage + " for case Id: ");
-    }
+
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
@@ -147,21 +53,10 @@ public class OutcomeController implements OutcomeApi {
   @Override
   public ResponseEntity<Void> spgNewStandalone(NewStandaloneAddress spgOutcome) throws GatewayException {
     gatewayEventManager.triggerEvent("", COMET_CESPGSTANDALONE_OUTCOME_RECEIVED, "transactionId",
-        spgOutcome.getTransactionId().toString(), "Case Ref", spgOutcome.getCaseReference(),
-        "Primary Outcome", spgOutcome.getPrimaryOutcomeDescription(), "Secondary Outcome",
+        spgOutcome.getTransactionId().toString(), "Primary Outcome", spgOutcome.getPrimaryOutcomeDescription(), "Secondary Outcome",
         spgOutcome.getSecondaryOutcomeDescription());
 
-    try {
-      String spgOutcomeToQueue = objectMapper.writeValueAsString(spgOutcome);
-      outcomeType = "SPGNS";
-      outcomePreprocessingProducer.sendOutcomeToPreprocessingQueue(spgOutcomeToQueue, "", outcomeType);
-    } catch (JsonProcessingException e) {
-      String errorMessage = "Unable to move SPG New Standalone outcome to pre-processing queue.";
-      gatewayEventManager.triggerErrorEvent(this.getClass(), e, errorMessage, "", FAILED_JSON_CONVERSION,
-              "Case Ref", spgOutcome.getCaseReference());
-      throw new GatewayException(GatewayException.Fault.SYSTEM_ERROR,
-          errorMessage + " for case Id: ");
-    }
+
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/spg/SPGExtraordinaryRefusalReceivedProcessor.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/spg/SPGExtraordinaryRefusalReceivedProcessor.java
@@ -2,16 +2,15 @@ package uk.gov.ons.census.fwmt.outcomeservice.converter.spg;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.fwmt.common.data.spg.SPGOutcome;
+import uk.gov.ons.census.fwmt.common.data.shared.CommonOutcome;
 import uk.gov.ons.census.fwmt.common.error.GatewayException;
 import uk.gov.ons.census.fwmt.events.component.GatewayEventManager;
-import uk.gov.ons.census.fwmt.outcomeservice.converter.SpgOutcomeServiceProcessor;
+import uk.gov.ons.census.fwmt.outcomeservice.converter.SPGOutcomeServiceProcessor;
 import uk.gov.ons.census.fwmt.outcomeservice.message.GatewayOutcomeProducer;
 import uk.gov.ons.census.fwmt.outcomeservice.template.TemplateCreator;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import static uk.gov.ons.census.fwmt.outcomeservice.config.GatewayEventsConfig.CESPG_OUTCOME_SENT;
 import static uk.gov.ons.census.fwmt.outcomeservice.enums.EventType.REFUSAL_RECEIVED;
@@ -27,7 +26,7 @@ public class SPGExtraordinaryRefusalReceivedProcessor implements SPGOutcomeServi
   private GatewayEventManager gatewayEventManager;
 
   @Override
-  public void processMessage(SPGOutcome spgOutcome) throws GatewayException {
+  public void processMessage(CommonOutcome spgOutcome) throws GatewayException {
     String eventDateTime = spgOutcome.getEventDate().toString();
     Map<String, Object> root = new HashMap<>();
     root.put("spgOutcome", spgOutcome);
@@ -40,6 +39,6 @@ public class SPGExtraordinaryRefusalReceivedProcessor implements SPGOutcomeServi
 
     gatewayOutcomeProducer.sendPropertyListing(outcomeEvent, String.valueOf(spgOutcome.getTransactionId()));
     gatewayEventManager.triggerEvent("caseId", CESPG_OUTCOME_SENT,
-        "type", CESPG_ADDRESS_NOT_VALID_OUTCOME_SENT, "transactionId", spgOutcome.getTransactionId().toString());
+        "type", "CESPG_EXTRAORDINARY_REFUSAL_RECEIVED_OUTCOME_SENT", "transactionId", spgOutcome.getTransactionId().toString());
   }
 }
