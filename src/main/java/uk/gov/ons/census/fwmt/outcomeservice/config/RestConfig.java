b4aac6b8b15b0f5ae93b7669e8d718a8ac1c8262
Simon <sdiaz1000@gmail.com>
2020-03-30 11:55:30 +0100

    added flag to ignore unrecognised json fields

---
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/config/RestConfig.java
@@ -0,0 +1,20 @@
+package uk.gov.ons.census.fwmt.outcomeservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class RestConfig {
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+	 MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+	 ObjectMapper objectMapper = new ObjectMapper();
+	 objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	 jsonConverter.setObjectMapper(objectMapper);
+	 return jsonConverter;
+	}
+}
