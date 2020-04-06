336921911d825c70896cda74bc95480560e6468a
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-04-06 16:36:12 +0100

    One missing annotation.

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/config/DateConfig.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/config/DateConfig.java
@@ -1,10 +1,12 @@
 package uk.gov.ons.census.fwmt.outcomeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
+@Configuration
 public class DateConfig {
   @Bean DateFormat dateFormat() {
     return new SimpleDateFormat("yyyy-MM-dd 'T'HH:mm:ss.SSSX");
