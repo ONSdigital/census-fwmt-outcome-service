054960eaa17b194c452e4262f36e2019ef723ed1
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2020-04-17 14:17:39 +0100

    Added the cancel feedback functionality.

---
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/spg/SpgCancelFeedbackProcessor.java
@@ -0,0 +1,36 @@
+package uk.gov.ons.census.fwmt.outcomeservice.converter.spg;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import uk.gov.ons.census.fwmt.common.error.GatewayException;
+import uk.gov.ons.census.fwmt.common.rm.dto.ActionInstructionType;
+import uk.gov.ons.census.fwmt.common.rm.dto.FwmtActionInstruction;
+import uk.gov.ons.census.fwmt.outcomeservice.converter.SpgOutcomeServiceProcessor;
+import uk.gov.ons.census.fwmt.outcomeservice.dto.SpgOutcomeSuperSetDto;
+import uk.gov.ons.census.fwmt.outcomeservice.message.RmFieldRepublishProducer;
+
+import java.util.UUID;
+
+@Slf4j
+@Component("CANCEL_FEEDBACK")
+public class SpgCancelFeedbackProcessor implements SpgOutcomeServiceProcessor {
+
+    @Autowired
+    private RmFieldRepublishProducer rmFieldRepublishProducer;
+
+    @Override
+    public UUID process(SpgOutcomeSuperSetDto outcome, UUID caseIdHolder) throws GatewayException {
+        FwmtActionInstruction fieldworkFollowup = FwmtActionInstruction.builder()
+                .actionInstruction(ActionInstructionType.UPDATE)
+                .surveyName("CENSUS")
+                .addressType("SPG")
+                .addressLevel("U")
+                .caseId(outcome.getCaseId().toString())
+                .build();
+
+        rmFieldRepublishProducer.republish(fieldworkFollowup);
+
+        return null;
+    }
+}
