26a6339ff6a782b569671d368cd1b680205de8bf
rameshons <ramesh.pydi@ext.ons.gov.uk>
2020-10-23 14:53:28 +0100

    RP: Outcome header event type CCS_ADDRESS_LISTED updated to message processors.

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/impl/PropertyListedCeProcessor.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/impl/PropertyListedCeProcessor.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static uk.gov.ons.census.fwmt.outcomeservice.enums.EventType.CCS;
+import static uk.gov.ons.census.fwmt.outcomeservice.enums.EventType.CCS_ADDRESS_LISTED;
 
 @Component("PROPERTY_LISTED_CE")
 public class PropertyListedCeProcessor implements OutcomeServiceProcessor {
@@ -63,13 +63,13 @@ public class PropertyListedCeProcessor implements OutcomeServiceProcessor {
     root.put("region",plCache.getOa().charAt(0));
 
 
-    String outcomeEvent = TemplateCreator.createOutcomeMessage(CCS, root);
+    String outcomeEvent = TemplateCreator.createOutcomeMessage(CCS_ADDRESS_LISTED, root);
 
     gatewayOutcomeProducer.sendOutcome(outcomeEvent, String.valueOf(outcome.getTransactionId()),
         GatewayOutcomeQueueConfig.GATEWAY_CCS_PROPERTY_LISTING_ROUTING_KEY);
     gatewayEventManager.triggerEvent(String.valueOf(caseId), OUTCOME_SENT,
         "survey type", type,
-        "type", CCS.toString(),
+        "type", CCS_ADDRESS_LISTED.toString(),
         "transactionId", outcome.getTransactionId().toString(),
         "routing key", GatewayOutcomeQueueConfig.GATEWAY_CCS_PROPERTY_LISTING_ROUTING_KEY);
 
