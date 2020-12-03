86885b6cd5ed1e74df7dda9ec70146cbac6fad44
jacobharrison <jacobharrison@outlook.com>
2020-12-03 12:27:18 +0000

    ppp-8139 - set org name in processor, simplfy templating

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/impl/PropertyListedCeProcessor.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/converter/impl/PropertyListedCeProcessor.java
@@ -63,7 +63,9 @@ public class PropertyListedCeProcessor implements OutcomeServiceProcessor {
     root.put("oa", plCache.getOa());
     root.put("region",plCache.getOa().charAt(0));
     root.put("estabType", outcome.getCeDetails() != null && outcome.getCeDetails().getEstablishmentType() != null ?
-        outcome.getCeDetails().getEstablishmentType() : "OTHER");
+        outcome.getCeDetails().getEstablishmentType() : "CE");
+    root.put("organisationName", outcome.getCeDetails() != null && outcome.getCeDetails().getEstablishmentName() != null ?
+        outcome.getCeDetails().getEstablishmentName() : "");
 
     String outcomeEvent = TemplateCreator.createOutcomeMessage(CCS_ADDRESS_LISTED, root);
 
