cdc5219c6c55bc67efa191feb08ce9eee543c226
Ramesh Pydi <ramesh.pydi@ext.ons.gov.uk>
2021-01-13 13:05:21 +0000

    FWMT-3061: Incorrect case id passed through NC endpoint

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverride.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverride.java
@@ -1,7 +1,6 @@
 package uk.gov.ons.census.fwmt.outcomeservice.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import uk.gov.ons.census.fwmt.common.data.nc.NCOutcome;
 import uk.gov.ons.census.fwmt.outcomeservice.data.GatewayCache;
