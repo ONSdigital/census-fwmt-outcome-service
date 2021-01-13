9ff6e97ebc0337e7d1cd70df52c7f4ea1d79eb47
SteveScorfield <steve.scorfield@ons.gov.uk>
2021-01-13 12:51:12 +0000

    Added code to try and fix the transaction issue being experience in WL. Unit tests are also updated

--- src/main/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverride.java
+++ src/main/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverride.java
@@ -1,20 +1,13 @@
 package uk.gov.ons.census.fwmt.outcomeservice.service.impl;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.fwmt.common.data.nc.NCOutcome;
 import uk.gov.ons.census.fwmt.outcomeservice.data.GatewayCache;
-
-import java.util.UUID;
-
 @Component
 public class NCTMCaseIdOverride {
-
     @Autowired
     GatewayCacheService gatewayCacheService;
-
-    public void overrideTMCaseIdWithRMOriginalCaseId(String caseID, NCOutcome ncOutcome) {
+    public String overrideTMCaseIdWithRMOriginalCaseId(String caseID) {
         GatewayCache gatewayCache = gatewayCacheService.getById(caseID);
-        ncOutcome.setCaseId(UUID.fromString(gatewayCache.getOriginalCaseId()));
+        return gatewayCache.getOriginalCaseId();
     }
 }
--- src/test/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverrideTest.java
+++ src/test/java/uk/gov/ons/census/fwmt/outcomeservice/service/impl/NCTMCaseIdOverrideTest.java
@@ -1,37 +1,32 @@
 package uk.gov.ons.census.fwmt.outcomeservice.service.impl;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-import uk.gov.ons.census.fwmt.common.data.nc.NCOutcome;
-import uk.gov.ons.census.fwmt.outcomeservice.data.GatewayCache;
+    import org.junit.Assert;
+    import org.junit.Test;
+    import org.junit.runner.RunWith;
+    import org.mockito.InjectMocks;
+    import org.mockito.Mock;
+    import org.mockito.junit.MockitoJUnitRunner;
+    import uk.gov.ons.census.fwmt.outcomeservice.data.GatewayCache;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+    import static org.mockito.ArgumentMatchers.anyString;
+    import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class NCTMCaseIdOverrideTest {
 
-    @InjectMocks
-    private NCTMCaseIdOverride nctmCaseIdOverride;
+  @InjectMocks
+  private NCTMCaseIdOverride nctmCaseIdOverride;
 
-    @Mock
-    private GatewayCacheService gatewayCacheService;
+  @Mock
+  private GatewayCacheService gatewayCacheService;
 
-    @Test
-    public void shouldOverrideTMCaseIdWithRMOriginalCaseId() {
-        GatewayCache gatewayCache = new GatewayCache();
-        gatewayCache.setOriginalCaseId("a48bf28e-e7f4-4467-a9fb-e000b6a55676");
-        NCOutcome ncOutcome = new NCOutcome();
-
-        when(gatewayCacheService.getById(anyString())).thenReturn(gatewayCache);
-        nctmCaseIdOverride.overrideTMCaseIdWithRMOriginalCaseId("b48bf28e-e7f4-4467-a9fb-e000b6a33543", ncOutcome);
-        assertThat("a48bf28e-e7f4-4467-a9fb-e000b6a55676", is(equalTo(ncOutcome.getCaseId().toString())));
-        verify(gatewayCacheService, times(1)).getById(anyString());
-    }
+  @Test
+  public void shouldOverrideTMCaseIdWithRMOriginalCaseId() {
+    GatewayCache gatewayCache = new GatewayCache();
+    gatewayCache.setOriginalCaseId("a48bf28e-e7f4-4467-a9fb-e000b6a55676");
+    when(gatewayCacheService.getById(anyString())).thenReturn(gatewayCache);
+    String hhCaseId = nctmCaseIdOverride.overrideTMCaseIdWithRMOriginalCaseId("b48bf28e-e7f4-4467-a9fb-e000b6a33543");
+    Assert.assertEquals("a48bf28e-e7f4-4467-a9fb-e000b6a55676",hhCaseId);
+    verify(gatewayCacheService, times(1)).getById(anyString());
+  }
 }
